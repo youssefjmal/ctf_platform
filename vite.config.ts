692bb31fde983842d9f1859815dcd2d36b9910ab
youssefjmel <youssefjmel42@gmail.com>
2025-12-22 17:13:05 +0100

    modified vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,4 +10,7 @@ export default defineConfig({
       },
     }),
   ],
-})
+  build: {
+    outDir: 'dist', // <-- this is the correct place for Vite build output
+  },
+});
